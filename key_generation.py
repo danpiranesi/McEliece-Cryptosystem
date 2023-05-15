449f80884dbf4496011d4b71a0828ae12df96a71
georgeb3ck <117672880+georgeb3ck@users.noreply.github.com>
2023-05-15 11:20:53 -0600

    plannning in key generation

---
+++ key_generation.py
@@ -0,0 +1,11 @@
+# be able to output a public key G^  and proivate key G,P,S
+
+
+
+# public key is the product of the three private keys
+
+# create a generating matrix G of size n by m that decodable with t errors
+
+#  create an inveritble  matrix S of size m by m
+
+#  create a permutation matrix of size n by n
