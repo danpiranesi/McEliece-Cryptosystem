720e301b9c1e5d8a7692a75da662eb6156790949
danpiranesi <110695726+danpiranesi@users.noreply.github.com>
2023-05-15 11:45:26 -0600

    Overview of decryption

---
+++ decryption.py
@@ -0,0 +1,21 @@
+# Dan
+
+# FLOW:
+# 1. compute inverse of P (P is a pemutation matrix)
+# 2. compute c-hat (c = P^(-1)c)
+# 3. use decoding algorithm A to decode c-hat to m-hat
+# 4. compute m = m-hat*S^(-1)
+
+def inversePerm(P): # takes in permuatation matrix and returns the inverse of it
+    pass
+
+def c_hat(c, inverseP): # multiplies c by result of inversePerm(P) and returns product
+    pass
+
+def decode(chat): # decodes c-hat to to m-hat 
+    pass
+
+def message(mhat, inverseS): # computes the message sent by Bob
+    pass
+
+
