60b4c1740eb7a4574046a10f208d0d663d0d8ed7
georgeb3ck <117672880+georgeb3ck@users.noreply.github.com>
2023-05-15 11:37:47 -0600

    outline for key_generation

--- key_generation.py
+++ key_generation.py
@@ -1,11 +1,35 @@
-# be able to output a public key G^  and proivate key G,P,S
+#Key generation
+
+#Goal, be able to output a public key (G^,t)  and private keys G,P,S
 
 
-
-# public key is the product of the three private keys
 
 # create a generating matrix G of size n by m that decodable with t errors
 
-#  create an inveritble  matrix S of size m by m
+def generatingMatrix():
+    pass
+    #return G , t
 
-#  create a permutation matrix of size n by n
+# create an inveritble  matrix S of size m by m
+
+def invertibleMatrix():
+
+    #return S
+    pass
+
+
+#  create a permutation matrix P of size n by n
+
+def permMatrix():
+
+    #return P
+    pass
+
+
+# put the tree parts together
+
+def generate_keys(): ##should we we able to input the size of the matrix you want????
+
+    #return G^, G, P, S, t
+    pass
+
