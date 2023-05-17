e512698f453235bc7147dd06dfb6143e0620b56b
georgeb3ck <117672880+georgeb3ck@users.noreply.github.com>
2023-05-16 21:45:45 -0600

    planning for invertible and permutation matrices

--- .ipynb_checkpoints/key_generation-checkpoint.py
+++ .ipynb_checkpoints/key_generation-checkpoint.py
@@ -1,6 +1,5 @@
 #Key generation
 import numpy as np
-
 #Goal, be able to output a public key (G^,t)  and private keys G,P,S
 
 
@@ -14,28 +13,14 @@ def generatingMatrix():
 # create an inveritble  matrix S of size m by m
 
 def invertibleMatrix():
-    #generate a random array S of size mxm
-    #try to find it's inverse
-    
-    # run numpy. linear algebra. inverse
-        #if we get annswer return S
-             #return S
 
-        #else try again with a differnt matrix
+    #return S
     pass
 
 
 #  create a permutation matrix P of size n by n
 
-def permMatrix(n):
-    #make a vector S of size n
-    #find a random permutation of the vector call it X
-
-    #run an algorithm to make the appropriate permutation matrix
-
-        #create an empty matrix P size nxn
-        #match the postion of the letter in the first vector to the row # of the second vector
-
+def permMatrix():
 
     #return P
     pass
