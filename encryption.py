931615a859e249ea8026159ed171a7930463468f
Chromezi <chromezi@hotmail.com>
2023-05-19 08:44:06 -0600

    Commit Testing

---
+++ encryption.py
@@ -0,0 +1,17 @@
+# Silvi 
+# May 15, 11:27
+
+# Note to self:
+# 非对称：公钥、私钥
+# 公钥可以通过同一线路传输
+# 私钥：g,a1,a2...an
+
+
+# ErrorVector
+def errorVec():
+    pass
+
+# Encryption 
+def encryption():
+    pass
+
