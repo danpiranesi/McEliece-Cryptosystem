6398a0c8f4c78b1e1403eda20d4fc74dd26c94f1
danpiranesi <110695726+danpiranesi@users.noreply.github.com>
2023-05-18 21:26:04 -0600

    Decryption (seems to be) working!

--- decryption.py
+++ decryption.py
@@ -1,31 +1,28 @@
-# FLOW:
-# 1. compute inverse of P (P is a pemutation matrix created in key_generation)
-# 2. compute c-hat (c = P^(-1)c)
-# 3. use decoding algorithm A to decode c-hat to m-hat
-# 4. compute m = m-hat*S^(-1)
-
 import numpy as np
-import random as rd
+
+def decrypt(ciphertext, permutation_matrix, non_singular_matrix):
+    # Step 1: Compute syndrome by multiplying inverse perm (matrix) and ciphertext (vector)
+    syndrome = np.matmul(inversePerm(permutation_matrix).T, ciphertext) % 2 # %2 to remain in binary field
+
+    # Step 2: Convert syndrome to original message by multiplying by inverse of non singular matrix made in key generation
+    decrypted_message = np.matmul(syndrome, np.linalg.inv(non_singular_matrix)) % 2 # %2 to remain in binary field
+
+    return decrypted_message
 
 def inversePerm(p): # takes in permuatation matrix and returns the inverse of it
-    # print("OG Perm Table")
-    # print(p)
     p = np.asanyarray(p)
     s = np.transpose(p)
     return s
 
-#P = np.array([[0,0,0,1],[1,0,0,0],[0,1,0,0],[0,0,1,0]])
-#print(inversePerm(P))
+# Example usage
+ciphertext = np.array([0, 1, 1])  # Example ciphertext from sender
+permutation_matrix = np.array([[0, 1, 0],
+                               [1, 0, 0],
+                               [0, 0, 1]])  # Example permutation matrix
 
-def c_hat(c, permtable): # multiplies c (ciphertext from Silvi) by result of inversePerm(P) and returns product
-    return inversePerm(permtable).dot(c)
+non_singular_matrix = np.array([[1, 0, 0],
+                                [0, 1, 0],
+                                [0, 0, 1]])  # Example non-singular matrix
 
-def decode(chat): # decodes c-hat to to m-hat 
-    pass
-
-def message(mhat, inverseS): # computes the message sent by Bob (m = m-hat*S^(-1))
-    pass
-
-randvector = np.array([i for i in range(4)])
-vector = np.random.permutation(randvector)
-
+original_message = decrypt(ciphertext, permutation_matrix, non_singular_matrix)
+print("Orignal message:", original_message) 
--- driver_code.py
+++ driver_code.py
@@ -1,4 +1,17 @@
 from key_generation import permMatrix
+from encryption import
 from decryption import inversePerm
+
+# call key generation methods
+# put public output onto a .txt file
+
+# input message to send
+# read .txt file to collect keys
+# encrypt message using methods
+# return ciphertext
+
+# take in ciphertext
+# run methods to decrypt message
+# return message
 
 print(inversePerm(permMatrix(5)))
