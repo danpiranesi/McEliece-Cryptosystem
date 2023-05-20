82b6cd444fdcaab1b00927431df896113f1eee40
georgeb3ck <117672880+georgeb3ck@users.noreply.github.com>
2023-05-20 11:08:17 -0600

    key gen using reedsolo package

--- key_generation.py
+++ key_generation.py
@@ -2,6 +2,9 @@
 from array import array
 import numpy as np
 
+import reedsolo as rs
+
+
 #Goal, be able to output a public key (G^,t)  and private keys G,P,S
 # create a generating matrix G of size n by k that decodable with t errors
 
@@ -10,9 +13,17 @@ def generatingMatrix(k,t): #create a reed-solomon generating matrix
     #t is number of error  t = (n-k)/2
     #caluculate an n based of t and l
     n = 2*t + k
+
+    #use reedsolo to create a generator poly
+    prim = rs.find_prime_polys(c_exp=12, fast_primes=True, single=True)
+    rs.init_tables(c_exp=12, prim=prim)
+
+    gen = rs.rs_generator_poly(n-1)
+    gen_poly = [int(i) for i in gen]
+    print(gen_poly)
    
     # generate a vector x of len n, values(0,1,2,3,4,...,n-1)
-    x = np.array([i for i in range(n)])
+    x = np.array(gen_poly)
 
     #generate the matrix G such that each row is x rasised to a that power
 
@@ -32,13 +43,13 @@ def generatingMatrix(k,t): #create a reed-solomon generating matrix
 
     
     #return G , n
-    return G, n
+    return G, n , gen
 
 # create an inveritble  matrix S of size k by k
 
 def invertibleMatrix(k):
     #generate a random array S of size kxk
-    rand_array = np.random.randint(2,size = (k,k))
+    rand_array = np.random.randint(2^4,size = (k,k))
     
     #try to find it's inverse
     try:
@@ -90,7 +101,7 @@ def generate_keys(k): ##should we we able to input the size of the matrix you wa
     #choose t, and k
     t=5
 
-    G , n = generatingMatrix(k,t)
+    G , n , gen = generatingMatrix(k,t)
 
     P = permMatrix(n)
 
@@ -98,8 +109,8 @@ def generate_keys(k): ##should we we able to input the size of the matrix you wa
 
     midstep = np.matmul(S,G) %2
     G_hat = np.matmul(midstep,P) %2
-    
-    return G_hat, G, P, S, t
+
+    return G_hat, G, P, S, t, gen
 
 
 #print(permMatrix(10))
